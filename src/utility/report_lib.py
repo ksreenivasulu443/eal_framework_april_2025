31e576fa70f061679aff191cc21003900bf54634
Admin <kattubadi426@gmail.com>
2025-07-16 06:33:41 +0530

    new branch

--- src/utility/report_lib.py
+++ src/utility/report_lib.py
@@ -7,22 +7,12 @@ report_dir = os.path.join(eal_path, "reports")
 os.makedirs(report_dir, exist_ok=True)
 
 # Create a single report filename for the session
-timestamp = datetime.datetime.now().strftime("%d%m%Y%H%M%S") #07072025072345
+timestamp = datetime.datetime.now().strftime("%d%m%Y%H%M%S")  #07072025072345
 report_filename = os.path.join(report_dir, f"report_{timestamp}.txt")
 print(report_filename)
 
 
-
-def write_output(validation_type, status, details):
+def write_output(validation_type, status, details, number_of_failures=0):
     # Write the output to the report file
     with open(report_filename, "a") as report:
         report.write(f"{validation_type}: {status}\nDetails: {details}\n\n")
-
-
-
-
-
-
-
-
-
